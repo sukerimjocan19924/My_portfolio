b88c56fa40d5598059d1c477d007ba5740e11594
HSR <hseoglan@naver.com>
2026-06-09 01:05:21 +0900

    home-about

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
+import About from '../../components/about/About'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <About/>
     </div>
   )
 }
