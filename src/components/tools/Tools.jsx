603a1afaa93cdd0128e4ed61b0b3cad2b6e3cd2b
HSR <hseoglan@naver.com>
2026-06-29 15:37:10 +0900

    home-ani&Media_tools

--- src/components/tools/Tools.jsx
+++ src/components/tools/Tools.jsx
@@ -3,27 +3,35 @@ import styles from './Tools.module.scss'
 import { MiniMascotIcon } from '../../utils/mascotIcon'
 import { MiniMascotIcon2_1, MiniMascotIcon2_2 } from '../../utils/mascotIcon'
 import { categories } from "../../utils/categories"
+import { motion as Motion, scale } from 'framer-motion'
+import { contentVariants, itemVariants, visualVariants } from '../../utils/variants'
+
 
 const Tools = () => {
   return (
     <section className={styles.tools} aria-labelledby="tools-title">
-      <div className={`inner ${styles.inner}`}>
-        <p className={styles.cardHeader}>02 / 나의 능력</p>
+      <Motion.div
+        variants={contentVariants}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ amount: 0.3 }}
+        className={`inner ${styles.inner}`}>
+        <Motion.p variants={itemVariants} className={styles.cardHeader}>02 / 나의 능력</Motion.p>
 
         <div className={styles.cardTitle}>
-          <p className={styles.title} id="tools-title">기술 스택</p>
-          <div className={styles.mascotWrapper}>
+          <Motion.p variants={itemVariants} className={styles.title} id="tools-title">기술 스택</Motion.p>
+          <Motion.div variants={visualVariants} className={styles.mascotWrapper}>
             <MiniMascotIcon2_1 />
             <MiniMascotIcon2_2 />
-          </div>
+          </Motion.div>
         </div>
 
         <div className={styles.grid}>
           {categories.map((cat) => (
-            <div key={cat.id} className={styles.card}>
+            <Motion.div variants={itemVariants} key={cat.id} className={styles.card}>
               <div className={styles.cardTop}>
                 <span className={styles.catIcon}>
-                  <cat.icon />
+                  <cat.icon className={styles.icon} />
                 </span>
                 <div className={styles.catMeta}>
                   <strong className={styles.catTitle}>{cat.title}</strong>
@@ -44,10 +52,10 @@ const Tools = () => {
                   </li>
                 ))}
               </ul>
-            </div>
+            </Motion.div>
           ))}
         </div>
-      </div>
+      </Motion.div>
     </section>
   )
 }
